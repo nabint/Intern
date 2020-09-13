0f9aee336820935a589d2c10603343f38fe9291e
nabint <nabinthapa36912@gmail.com>
2020-09-13 14:45:04 +0545

    Improved Userposts and UserComment UI

--- lib/pages/userComments.dart
+++ lib/pages/userComments.dart
@@ -6,25 +6,50 @@ class UserCommentsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Center(
+          child: Text(
+            "Comments",
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+      ),
       body: ListView.builder(
         itemCount: commentlist.length,
         itemBuilder: (context, index) {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text('Name ' + commentlist[index].name),
-              SizedBox(
-                height: 7,
-              ),
-              Text('Email ' + commentlist[index].email),
-              SizedBox(
-                height: 7,
-              ),
-              Text('Body ' + commentlist[index].body),
-              SizedBox(
-                height: 30,
-              ),
-            ],
+          return Card(
+            elevation: 5.0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  child: Text(
+                    commentlist[index].name,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(fontSize: 20, color: Colors.blue),
+                  ),
+                ),
+                SizedBox(
+                  height: 7,
+                ),
+                Text(
+                  commentlist[index].email,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontSize: 20, color: Colors.blue),
+                ),
+                SizedBox(
+                  height: 7,
+                ),
+                Text(
+                  commentlist[index].body,
+                  style: TextStyle(fontSize: 15, color: Colors.black),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+              ],
+            ),
           );
         },
       ),
--- lib/pages/userPosts.dart
+++ lib/pages/userPosts.dart
@@ -9,20 +9,43 @@ class UserPostsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Center(
+            child: Text(
+              "Posts",
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+        ),
         body: ListView.builder(
-      itemCount: postlist.length,
-      itemBuilder: (context, index) {
-        return ListTile(
-          title: Text(postlist[index].title),
-          subtitle: Text(postlist[index].body),
-          onTap: () {
-            userlistBloc.add(FetchUserComments(
-              postlist[index].id,
-            ));
+          itemCount: postlist.length,
+          itemBuilder: (context, index) {
+            return Card(
+              elevation: 5.0,
+              child: ListTile(
+                title: Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Container(
+                      child: Text(
+                    postlist[index].title,
+                    style: TextStyle(fontSize: 20),
+                    overflow: TextOverflow.ellipsis,
+                  )),
+                ),
+                subtitle: Text(
+                  postlist[index].body,
+                  style: TextStyle(fontSize: 15),
+                ),
+                onTap: () {
+                  userlistBloc.add(FetchUserComments(
+                    postlist[index].id,
+                  ));
+                },
+              ),
+            );
+            // return Text(postlist[index].title);
           },
-        );
-        // return Text(postlist[index].title);
-      },
-    ));
+        ));
   }
 }
